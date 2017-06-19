0c63b3eac499c0727e815b6a7e0ccc3a5224e3b9
zcong1993 <1005997919@qq.com>
2017-06-19 21:16:11 +0800

    tweak build script

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
-COMMIT = $$(git describe --always)
 
 generate:
 	@go generate ./...
 
 build: generate
-	@echo "====> Build gost-cli in ./bin "
-	go build -ldflags "-X main.GitCommit=\"$(COMMIT)\"" -o bin/gost
+	@echo "====> Build gost-cli in ./pkg "
+	./build.sh
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+XC_ARCH=${XC_ARCH:-amd64}
+XC_OS=${XC_OS:-darwin linux windows}
+
+COMMIT=`git describe --always`
+
+
+rm -rf pkg/
+gox \
+    -ldflags "-X main.GitCommit=${COMMIT}" \
+    -parallel=-1 \
+    -os="${XC_OS}" \
+    -arch="${XC_ARCH}" \
+    -output "pkg/gost-{{.OS}}"
