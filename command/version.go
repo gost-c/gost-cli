dd138bde79178b90ba53800466117353542fba3e
zcong1993 <1005997919@qq.com>
2018-01-03 16:33:40 +0800

    remove version updater

--- command/version.go
+++ command/version.go
@@ -1,67 +1,38 @@
 package command
 
 import (
-	"bytes"
 	"fmt"
-	latest "github.com/tcnksm/go-latest"
-	"time"
 )
 
-const defaultCheckTimeout = 2 * time.Second
+// Name is cli's name
+const Name = "gost"
 
-// VersionCommand is struct of version meta
+// Version is cli's current version
+const Version = "v1.2.1"
+
+// GitCommit describes latest commit hash.
+// This value is extracted by git command when building.
+// To set this from outside, use go build -ldflags "-X main.GitCommit \"$(COMMIT)\""
+var GitCommit string
+
+// VersionCommand is struct of login meta
 type VersionCommand struct {
 	Meta
-
-	Name     string
-	Version  string
-	Revision string
 }
 
 // Run is entry function of version command
 func (c *VersionCommand) Run(args []string) int {
-	var versionString bytes.Buffer
-
-	fmt.Fprintf(&versionString, "%s version %s", c.Name, c.Version)
-	if c.Revision != "" {
-		fmt.Fprintf(&versionString, " (%s)", c.Revision)
+	version := fmt.Sprintf("\n%s version %s", Name, Version)
+	if len(GitCommit) != 0 {
+		version += fmt.Sprintf(" (%s)", GitCommit)
 	}
-
-	c.UI.Output(versionString.String())
-	var buf bytes.Buffer
-	verCheckCh := make(chan *latest.CheckResponse)
-	go func() {
-		fixFunc := latest.DeleteFrontV()
-		githubTag := &latest.GithubTag{
-			Owner:             "gost-c",
-			Repository:        "gost-cli",
-			FixVersionStrFunc: fixFunc,
-		}
-
-		res, err := latest.Check(githubTag, fixFunc(c.Version))
-		if err != nil {
-			// Don't return error
-			return
-		}
-		verCheckCh <- res
-	}()
-
-	select {
-	case <-time.After(defaultCheckTimeout):
-	case res := <-verCheckCh:
-		if res.Outdated {
-			fmt.Fprintf(&buf,
-				"Latest version of %s is v%s, please upgrade!\n",
-				c.Name, res.Current)
-		}
-	}
-	c.UI.Output(buf.String())
+	fmt.Println(version)
 	return 0
 }
 
 // Synopsis is description of version command
 func (c *VersionCommand) Synopsis() string {
-	return fmt.Sprintf("Print %s version and quit", c.Name)
+	return fmt.Sprintf("Print %s version and quit", Name)
 }
 
 // Help is help message of version command
