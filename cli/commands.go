cb4a51150c60c67fe3911ed57c1998b568600adf
zcong1993 <1005997919@qq.com>
2018-01-03 16:40:30 +0800

    remove version updater

--- cli/commands.go
+++ cli/commands.go
@@ -32,9 +32,6 @@ func Commands(meta *command.Meta) map[string]cli.CommandFactory {
 		"version": func() (cli.Command, error) {
 			return &command.VersionCommand{
 				Meta:     *meta,
-				Version:  Version,
-				Revision: GitCommit,
-				Name:     Name,
 			}, nil
 		},
 	}
