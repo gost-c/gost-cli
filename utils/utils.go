2f1e8ee78d880ca61fc5764d4464b782dc6f2052
zcong1993 <1005997919@qq.com>
2018-01-16 15:21:53 +0800

    dev: Skip folder and limit file size, fix

--- utils/utils.go
+++ utils/utils.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/dustin/go-humanize"
 	"github.com/gost-c/gost-cli/colors"
 	"github.com/mitchellh/go-homedir"
 	"io/ioutil"
@@ -20,7 +22,20 @@ var (
 	WebURL = "https://gost.zcong.moe/#/gost/"
 	//WebURL = "http://localhost:3000/#/gost/"
 	space = "  "
+	// MaxSize is single file max size, 10k
+	MaxSize      = int64(1024 * 10)
+	MaxSizeHuman = humanize.Bytes(uint64(MaxSize))
 )
+
+// PathStat is file stat
+type PathStat struct {
+	// Error is error message
+	Error error
+	// IsFolder show if the PathStat is folder
+	IsFolder bool
+	// Size is file humanized size if is a file
+	Size string
+}
 
 // ConfigFile is the true path of config file `.gostrc`
 var ConfigFile string
@@ -77,4 +92,30 @@ func DoRequest(method, url string, v interface{}, headers map[string]string) err
 	defer resp.Body.Close()
 	err = json.NewDecoder(resp.Body).Decode(v)
 	return err
+}
+
+// GetPathStat return the path type, if is file return file size
+func GetPathStat(p string) *PathStat {
+	fileInfo, err := os.Stat(p)
+	ps := &PathStat{}
+	if err != nil {
+		ps.Error = err
+		return ps
+	}
+	if fileInfo.IsDir() {
+		ps.IsFolder = true
+		return ps
+	}
+	s := fileInfo.Size()
+	if s > MaxSize {
+		ps.Error = errFileTooBig(s)
+		return ps
+	}
+	ps.IsFolder = false
+	ps.Size = humanize.Bytes(uint64(fileInfo.Size()))
+	return ps
+}
+
+func errFileTooBig(s int64) error {
+	return errors.New(fmt.Sprintf("File is too big %s, max allowed size is %s.", humanize.Bytes(uint64(s)), MaxSizeHuman))
 }
---
+++ utils/utils_test.go
@@ -0,0 +1,10 @@
+package utils_test
+
+import (
+	"github.com/gost-c/gost-cli/utils"
+	"testing"
+)
+
+func TestFileOrFolder(t *testing.T) {
+	t.Log(utils.FileOrFolder("../circle.yml"))
+}
